a8aaff86e487a12a4c2e649772dad0ee4bd082b9
bobbidigi <bobbidigi34@gmail.com>
2019-03-11 13:25:08 -0400

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,69 @@
-var path = require('path');
+/* global __dirname */ // to stop the linter complaining
+
+// In webpack.config.js
 var HtmlWebpackPlugin = require('html-webpack-plugin');
-
-
+var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + '/app/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
 module.exports = {
-  entry: './app/index.js',
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'index_bundle.js',
-      publicPath: '/'     
+  devtool: 'source-map',
+  entry: [
+    './app/index.js'
+  ],
+  devServer: {
+    inline: true,
+    port: 3002
+  },
+  output: {
+    path: __dirname + '/dist',
+    filename: 'index_bundle.js'
   },
   module: {
-    rules: [
-      { test: /\.(js)$/, use: 'babel-loader'},
-      { test: /\.css$/, use: [ 'style-loader', 'css-loader'] }
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      }
     ]
   },
-  mode: 'development',
-  devServer: {
-    historyApiFallback: true
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'app/index.html'
-    })
-  ]
+  plugins: [HTMLWebpackPluginConfig]
 };
+
+
+
+
+
+// var path = require('path');
+// var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+// module.exports = {
+//   entry: './app/index.js',
+//     output: {
+//       path: path.resolve(__dirname, 'dist'),
+//       filename: 'index_bundle.js',
+//       publicPath: '/'     
+//   },
+//   module: {
+//     rules: [
+//       { test: /\.(js)$/, use: 'babel-loader'},
+//       { test: /\.css$/, use: [ 'style-loader', 'css-loader'] }
+//     ]
+//   },
+//   mode: 'development',
+//   devServer: {
+//     historyApiFallback: true
+//   },
+//   plugins: [
+//     new HtmlWebpackPlugin({
+//       template: 'app/index.html'
+//     })
+//   ]
+// };
