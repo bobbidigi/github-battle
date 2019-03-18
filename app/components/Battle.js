ef3e62547fd56d4045d2c1aa51062a42ca5af24c
bobbidigi <bobbidigi34@gmail.com>
2019-03-17 20:56:37 -0400

    Updates

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,34 +1,8 @@
 var React = require('react');
 var PropTypes = require('prop-types');
 var Link = require('react-router-dom').Link;
+var PlayerPreview = require('./PlayerPreview');
 
-function PlayerPreview(props){
-  return(
-    <div className="player-preview">
-      <div>
-        <img
-        className='avatar'
-        src={props.avatar}
-        alt={'avatar for ' + props.username}
-        />
-        <h2 className='username'>@{props.username}</h2>
-      </div>
-      <button
-        className='reset'
-        onClick={props.onReset.bind(null, props.id)}
-      >
-        Reset
-      </button>
-    </div>
-  )
-}
-
-PlayerPreview.propTypes = {
-  avatar: PropTypes.string.isRequired,
-  username: PropTypes.string.isRequired,
-  id: PropTypes.string.isRequired,
-  onReset: PropTypes.func.isRequired
-}
 
 class PlayerInput extends React.Component {
   constructor(props) {
@@ -146,10 +120,13 @@ class Battle extends React.Component {
           <PlayerPreview
           avatar={playerOneImage}
           username={playerOneName}
-          onReset={this.handleReset}
-          id='playerOne'
-          />
-        }
+          >
+            <button
+              className='reset'
+              onClick={this.handleReset.bind(null, 'playerOne')}>
+              Reset
+            </button>
+          </PlayerPreview>}
 
         {!playerTwoName &&
           <PlayerInput 
@@ -163,10 +140,13 @@ class Battle extends React.Component {
           <PlayerPreview
             avatar={playerTwoImage}
             username={playerTwoName}
-            onReset={this.handleReset}
-            id='playerTwo'
-          />
-        }
+          >
+            <button
+              className='reset'
+              onClick={this.handleReset.bind(null, 'playerTwo')}>
+              Reset
+            </button>
+          </PlayerPreview>}
       </div>
       {playerOneImage && playerTwoImage &&
         <Link
